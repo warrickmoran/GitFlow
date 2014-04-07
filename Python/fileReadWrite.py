3001900584e70bf9d004f887893a71bd1dd7e263
Warrick Moran <Warrick.Moran@noaa.gov>
2014-04-07 12:31:10 -0400

    add filename to function calls

--- Python/fileReadWrite.py
+++ Python/fileReadWrite.py
@@ -1,28 +1,30 @@
 __author__ = 'warrickmoran'
+#
+# General Read/Write methods from Python 101
+#
 
-
-def file_read():
+def file_read(filename):
     """
     Read File Contents
     """
     try:
-        with open('data/text.txt', 'r') as handler:
+        with open(filename, 'r') as handler:
             data = handler.readlines() # read ALL the lines!
             print(data)
     except IOError:
         print("An IOError has occurred!")
 
 
-def file_write():
+def file_write(filename):
     """
     Write File
     """
     try:
-        with open('data/output.txt','w') as handler:
+        with open(filename,'w') as handler:
             handler.write("This is a test!")
     except IOError:
         print("An IOError has occurred!")
 
 
-file_read()
-file_write()
+file_read('data/text.txt')
+file_write('data/output.txt')
