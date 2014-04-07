c6f02abd6c0338509d4fcbad0bef860b8c00be49
Warrick Moran <Warrick.Moran@noaa.gov>
2014-04-07 12:16:49 -0400

    utilize "with" method to finalize resource

--- Python/fileReadWrite.py
+++ Python/fileReadWrite.py
@@ -5,22 +5,20 @@ def fileRead():
     Read File Contents
     """
     try:
-        handler = open('data/text.txt', 'r')
-        data = handler.readlines() # read ALL the lines!
-        print(data)
+        with open('data/text.txt', 'r') as handler:
+            data = handler.readlines() # read ALL the lines!
+            print(data)
     except IOError:
         print("An IOError has occurred!")
-    finally:
-        handler.close()
+
 
 def fileWrite():
     try:
-        handler = open('data/output.txt','w')
-        handler.write("This is a test!")
+        with open('data/output.txt','w') as handler:
+            handler.write("This is a test!")
     except IOError:
         print("An IOError has occurred!")
-    finally:
-        handler.close()
+
 
 fileRead()
 fileWrite()
