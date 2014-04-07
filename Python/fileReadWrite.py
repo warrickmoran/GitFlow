652f03838a94b0e748350b80e96e7686f2f994b6
Warrick Moran <Warrick.Moran@noaa.gov>
2014-04-07 12:10:14 -0400

    format check - function names should be lower case

--- Python/fileReadWrite.py
+++ Python/fileReadWrite.py
@@ -1,15 +1,15 @@
 __author__ = 'warrickmoran'
 
-def fileRead():
+def file_read():
     handler = open('data/text.txt', 'r')
     data = handler.readlines() # read ALL the lines!
     print(data)
     handler.close()
 
-def fileWrite():
+def file_write():
     handler = open('data/output.txt','w')
     handler.write("This is a test!")
     handler.close()
 
-fileRead()
-fileWrite()
+file_read()
+file_write()
